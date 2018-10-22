d2c0b14252958a6029a5c9e65221d6a05818177c
Savin Kumar <savinkumarn@gmail.com>
2018-10-22 07:06:32 +0530

    New Commit

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 #This is the main program to invoke the practice scripts
 from pythonSpecial.main_special import main_special
+from dataStructures.main_dataStruct import main_ds
 
 def main():
     # main function for special functions
     main_special()
+
+    # main function for data structure testing
+    main_ds()
 
 
 if __name__ == '__main__':
